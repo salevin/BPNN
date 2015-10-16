9ff6449c3fd0e50d9094b02c8fffe43ebec63a8a
salevin <samueljlevin@gmail.com>
2015-10-15 22:11:57 -0400

    Total change, but working

--- main.py
+++ main.py
@@ -18,172 +18,188 @@ So to encode (1,0,1,1) you'd multiply (1, 0, 1, 1) G and get (1,0,1,1,0,1,0)  [a
 take remainder of dividing by 2 after you do anything.]
 """
 
-import os
-import sys
+# Huge Credit to honnibal
+# Made off of https://gist.github.com/honnibal/6a9e5ef2921c0214eeeb
+
+
 import timeit
 
 import numpy
-
 import theano
 import theano.tensor as T
+import random
 
 __author__ = 'slevin'
 
 
-class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None, activation=T.tanh):
-        """
-        Typical hidden layer of a MLP: units are fully-connected and have
-        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
-        and the bias vector b is of shape (n_out,).
+def make_data():
+    train_set = _make_xor(900)
+    valid_set = _make_xor(10000)
+    test_set = _make_xor(500)
 
-        NOTE : The nonlinearity used here is tanh
+    return _make_array(train_set), _make_array(valid_set), _make_array(test_set)
 
-        Hidden unit activation is given by: tanh(dot(input,W) + b)
 
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
+def _make_array(xy):
+    data_x, data_y = xy
+    return zip(
+        numpy.asarray(data_x, dtype=theano.config.floatX),
+        numpy.asarray(data_y, dtype='int32'))
 
-        :type input: theano.tensor.dmatrix
-        :param input: a symbolic tensor of shape (n_examples, n_in)
 
-        :type n_in: int
-        :param n_in: dimensionality of input
+def _make_xor(size):
+    x = []
+    y = []
+    for i in range(size):
+        x.append([random.randint(0, 1), random.randint(0, 1)])
+        y.append(x[i][0] ^ x[i][1])
+    return x, y
 
-        :type n_out: int
-        :param n_out: number of hidden units
 
-        :type activation: theano.Op or function
-        :param activation: Non linearity to be applied in the hidden
-                           layer
-        """
-        self.input = input
+def _init_logreg_weights(n_hidden, n_out):
+    weights = numpy.zeros((n_hidden, n_out), dtype=theano.config.floatX)
+    bias = numpy.zeros((10,), dtype=theano.config.floatX)
+    return (
+        theano.shared(name='W', borrow=True, value=weights),
+        theano.shared(name='b', borrow=True, value=bias)
+    )
 
-        # `W` is initialized with `W_values` which is uniformely sampled
-        # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
-        # for tanh activation function
-        # the output of uniform if converted using asarray to dtype
-        # theano.config.floatX so that the code is runable on GPU
-        # Note : optimal initialization of weights is dependent on the
-        #        activation function used (among other things).
-        #        For example, results presented in [Xavier10] suggest that you
-        #        should use 4 times larger initial weights for sigmoid
-        #        compared to tanh
-        #        We have no info for other function, so we use the same as
-        #        tanh.
-        if W is None:
-            W_values = numpy.asarray(
-                rng.uniform(
-                    low=-numpy.sqrt(6. / (n_in + n_out)),
-                    high=numpy.sqrt(6. / (n_in + n_out)),
-                    size=(n_in, n_out)
-                ),
-                dtype=theano.config.floatX
-            )
-            if activation == theano.tensor.nnet.sigmoid:
-                W_values *= 4
 
-            W = theano.shared(value=W_values, name='W', borrow=True)
+def _init_hidden_weights(n_in, n_out):
+    rng = numpy.random.RandomState(1234)
+    weights = numpy.asarray(
+        rng.uniform(
+            low=-numpy.sqrt(6. / (n_in + n_out)),
+            high=numpy.sqrt(6. / (n_in + n_out)),
+            size=(n_in, n_out)
+        ),
+        dtype=theano.config.floatX
+    )
+    bias = numpy.zeros((n_out,), dtype=theano.config.floatX)
+    return (
+        theano.shared(value=weights, name='W', borrow=True),
+        theano.shared(value=bias, name='b', borrow=True)
+    )
 
-        if b is None:
-            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
-            b = theano.shared(value=b_values, name='b', borrow=True)
 
-        self.W = W
-        self.b = b
+# Define how an input is fed through a layer of the network, and how a step of
+# the stochastic gradient descent is computed.
 
-        lin_output = T.dot(input, self.W) + self.b
-        self.output = (
-            lin_output if activation is None
-            else activation(lin_output)
-        )
-        # parameters of the model
-        self.params = [self.W, self.b]
+# Note that these are *symbolic expressions* --- we are just compiling code here.
+# These functions are only called during compile_model.  The *actual* feed-forward
+# and SGD update procedures, which happen iteratively on each example, are
+# Theano-internal.
+def feed_forward(activation, weights, bias, input_):
+    return activation(T.dot(input_, weights) + bias)
 
-    class MLP(object):
-        """Multi-Layer Perceptron Class
 
-        A multilayer perceptron is a feedforward artificial neural network model
-        that has one layer or more of hidden units and nonlinear activations.
-        Intermediate layers usually have as activation function tanh or the
-        sigmoid function (defined here by a ``HiddenLayer`` class)  while the
-        top layer is a softmax layer (defined here by a ``LogisticRegression``
-        class).
-        """
+def sgd_step(param, cost, learning_rate):
+    return param - (learning_rate * T.grad(cost, param))
 
-        def __init__(self, rng, input, n_in, n_hidden, n_out):
-            """Initialize the parameters for the multilayer perceptron
 
-            :type rng: numpy.random.RandomState
-            :param rng: a random number generator used to initialize weights
+# These are also symbolic.
+def L1(L1_reg, w1, w2):
+    return L1_reg * (abs(w1).sum() + abs(w2).sum())
 
-            :type input: theano.tensor.TensorType
-            :param input: symbolic variable that describes the input of the
-            architecture (one minibatch)
 
-            :type n_in: int
-            :param n_in: number of input units, the dimension of the space in
-            which the datapoints lie
+def L2(L2_reg, w1, w2):
+    return L2_reg * ((w1 ** 2).sum() + (w2 ** 2).sum())
 
-            :type n_hidden: int
-            :param n_hidden: number of hidden units
 
-            :type n_out: int
-            :param n_out: number of output units, the dimension of the space in
-            which the labels lie
+def compile_model(n_in, n_classes, n_hidden, learning_rate, L1_reg, L2_reg):
+    '''Compile train and evaluation functions, which we'll then call iteratively
+    to train the parameters.  This function is called exactly once --- think of
+    it like a compiler.  We declare variables, allocate memory, and define some
+    computation.
+    '''
+    # allocate symbolic variables for the data
+    x = T.vector('x')  # Features
+    y = T.iscalar('y')  # (Gold) Label
 
-            """
+    # Allocate and initialize weights.  These are stored internally, and updated.
+    hidden_W, hidden_b = _init_hidden_weights(n_in, n_hidden)
+    logreg_W, logreg_b = _init_logreg_weights(n_hidden, n_classes)
 
-            # Since we are dealing with a one hidden layer MLP, this will translate
-            # into a HiddenLayer with a tanh activation function connected to the
-            # LogisticRegression layer; the activation function can be replaced by
-            # sigmoid or any other nonlinear function
-            self.hiddenLayer = HiddenLayer(
-                rng=rng,
-                input=input,
-                n_in=n_in,
-                n_out=n_hidden,
-                activation=T.tanh
-            )
+    # Estimate P(y | x) given the current weights
+    p_y_given_x = feed_forward(
+        T.nnet.softmax,
+        logreg_W,
+        logreg_b,
+        feed_forward(
+            T.tanh,
+            hidden_W,
+            hidden_b,
+            x))  # <--- Our input variable (the features)
 
-            # The logistic regression layer gets as input the hidden units
-            # of the hidden layer
-            self.logRegressionLayer = LogisticRegression(
-                input=self.hiddenLayer.output,
-                n_in=n_hidden,
-                n_out=n_out
-            )
-            # end-snippet-2 start-snippet-3
-            # L1 norm ; one regularization option is to enforce L1 norm to
-            # be small
-            self.L1 = (
-                abs(self.hiddenLayer.W).sum()
-                + abs(self.logRegressionLayer.W).sum()
-            )
+    cost = (
+        -T.log(p_y_given_x[0, y])  # <-- Negative log likelihood of gold label
+        + L1(L1_reg, logreg_W, hidden_W)
+        + L2(L2_reg, logreg_W, hidden_W)
+    )
 
-            # square of L2 norm ; one regularization option is to enforce
-            # square of L2 norm to be small
-            self.L2_sqr = (
-                (self.hiddenLayer.W ** 2).sum()
-                + (self.logRegressionLayer.W ** 2).sum()
-            )
+    # Compile the training function.  Successive calls to this update the weights.
+    # Internal state is maintained.
+    # The output is "cost", which requires the computation of p_y_given_x.  We
+    # also define how to update the weights based on the input label.
+    train_model = theano.function(
+        inputs=[x, y],
+        outputs=cost,  # <-- Output depends on cost, which depends on P(y | x)
+        updates=[
+            (logreg_W, sgd_step(logreg_W, cost, learning_rate)),
+            (logreg_b, sgd_step(logreg_b, cost, learning_rate)),
+            (hidden_W, sgd_step(hidden_W, cost, learning_rate)),
+            (hidden_b, sgd_step(hidden_b, cost, learning_rate)),
+        ]
+    )
 
-            # negative log likelihood of the MLP is given by the negative
-            # log likelihood of the output of the model, computed in the
-            # logistic regression layer
-            self.negative_log_likelihood = (
-                self.logRegressionLayer.negative_log_likelihood
-            )
-            # same holds for the function computing the number of errors
-            self.errors = self.logRegressionLayer.errors
+    # Compile the evaluation function, which returns a 0/1 loss wrt the true
+    # label.  Note that the output depends on p_y_given_x, so the program must
+    # compute it.
+    evaluate_model = theano.function(
+        inputs=[x, y],
+        outputs=T.neq(y, T.argmax(p_y_given_x[0])),
+    )
+    return train_model, evaluate_model
 
-            # the parameters of the model are the parameters of the two layer it is
-            # made out of
-            self.params = self.hiddenLayer.params + self.logRegressionLayer.params
-            # end-snippet-3
 
-            # keep track of model input
-            self.input = input
+def main(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=30,
+         n_hidden=500):
+    train_examples, dev_examples, test_examples = make_data()
+    print '... building the model'
+    train_model, evaluate_model = compile_model(2 * 1, 10, n_hidden, learning_rate, L1_reg, L2_reg)
+    print '... training'
+    start = timeit.default_timer()
+    for epoch in range(1, n_epochs + 1):
+        for x, y in train_examples:
+            train_model(x, y)
+        # compute zero-one loss on validation set
+        error = numpy.mean([evaluate_model(x, y) for x, y in dev_examples])
+        print('epoch %i, validation error %f %%' % (epoch, error * 100))
+    end = timeit.default_timer()
+    print("Finished Ran for %i for seconds and average of %f seconds per epoch" % ((end - start), ((end - start)/n_epochs)))
 
-    if __name__ == '__main__':
-        print("hey")
+    go_on = True
+    while go_on:
+        x1 = input("First binary input: ")
+        x2 = input("Second binary input: ")
+        if evaluate_model([x1, x2], 1) == 0:
+            ans = 1
+        else:
+            ans = 0
+        print(ans)
+
+        want = raw_input("Want to continue? (y/n): ")
+
+        chosen = False
+        while not chosen:
+            if want.lower() == 'n':
+                go_on = False
+                chosen = True
+            elif want.lower() != 'y':
+                want = raw_input("What was that? (y/n): ")
+            else:
+                chosen = True
+
+
+if __name__ == '__main__':
+    main()
